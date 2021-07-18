5dd6b45a0c1cf663e70c3b93f8a6001598e711d2
Dalaias <dalaiaas1327@gmail.com>
2021-07-18 16:40:11 -0600

    subiendo archivo tarea4 c++

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+     int n;
+     
+     
+     cout<<" ingrese un numero:"<<endl;
+     cin>>n;
+     cout<<endl;
+     
+     for(int i=10;i>=0;i--) {
+      cout<<n<<" x "<<i<<" = "<<n*1<<endl;
+      
+	 }
+
+
+	return 0;
+}
